8befbcd8787347db5bb15c8b231f458485941e10
S3-015 <wmc015@pm.me>
2020-11-23 19:44:01 -0500

    move to Postgres

--- app.py
+++ app.py
@@ -1,43 +1,63 @@
-from flask import Flask, redirect
-import requests as r 
-from requests.exceptions import TooManyRedirects
-# import wikipediaapi as wiki
-from random import random
-
+import random
 import re
 
+from flask import Flask, redirect
+from flask_sqlalchemy import SQLAlchemy
+import requests as r
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@/wiki'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    pageid = db.Column(db.Integer, unique=True, nullable=False)
+    pagecount = db.Column(db.Integer, nullable=False)
+
+
+class CatLink(db.Model):
+    __tablename__ = 'catlinks'
+    parent_id = db.Column(db.Integer, db.ForeignKey('categories.pageid'), primary_key=True)
+    child_id = db.Column(db.Integer, db.ForeignKey('categories.pageid'), primary_key=True)
+
+
+def request_title(pageid):
+    params = {'action': 'query',
+              'pageids': str(pageid),
+              'prop': 'title',
+              'format': 'json'}
+
+    req = r.get('https://en.wikipedia.org/w/api.php', params).json()
+    return req['query']['pages'][str(pageid)]['title']
+
+def request_id(title):
+    params = {'action': 'query',
+              'titles': str(title),
+              'prop': 'pageid',
+              'format': 'json'}
+
+    req = r.get('https://en.wikipedia.org/w/api.php', params).json()
+    return int(list(req['query']['pages'].keys())[0])
+
 
 @app.route('/<category>')
-def redirect_to_category(category, depth=0, breadcrumbs=[]):
-    url = f"https://en.wikipedia.org/wiki/Special:RandomInCategory/{category}"
-    if depth > 10:
-        raise TooManyRedirects()
-    
-    resolved_url = r.get(url).url
-    # Test if this itself is a special page
-    special_match = re.match("https://en.wikipedia.org/wiki/([^:]+):(.+)", resolved_url)
-    if special_match:
-        special_type = special_match.group(1)
-        special_keyword = special_match.group(2)
-        print(f"found special match {special_type}:{special_keyword}")
+def random_in_category(category):
+    catid = request_id(f"Category:{category}")
+    cats = Category.query.filter((CatLink.parent_id == catid) &
+                                 (Category.pageid == CatLink.child_id)).all()
 
-        if special_type == "Book":
-            print(f"")
-            return
-        elif special_type == "Category":
-            print(f"recursing on category {special_keyword}")
-            breadcrumbs.append(special_keyword.replace("_", " "))
-            return redirect_to_category(special_keyword, depth=depth+1, breadcrumbs=breadcrumbs)
-        else:
-            print(f"don't know what to do with this, retrying with category {category}...")
-            return redirect_to_category(category, depth=depth+1, breadcrumbs=breadcrumbs)
-    else:
-        if random() < (0.5 ** (depth + 1)):
-            print(f"don't like this page, trying again with category {category}...")
-            return redirect_to_category(category, depth=depth+1, breadcrumbs=breadcrumbs)
+    ids = [cat.pageid for cat in cats]
+    counts = [cat.pagecount for cat in cats]
 
-        print(" > ".join(breadcrumbs))
-        breadcrumbs = []
-        return redirect(resolved_url)
+    destid = random.choices(ids, counts)[0]
+    print(destid)
+    destname = request_title(destid)
+    print(destname)
 
+    dest = None
+    while dest is None or "/Category:" in dest.url:
+        dest = r.get(f"https://en.wikipedia.org/wiki/Special:RandomInCategory/{destname}")
+    return redirect(dest.url)
--- default.nix
+++ default.nix
@@ -1,10 +1,13 @@
 let pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
-	buildInputs = with pkgs.python3Packages; [
-		black
-		flask
-		requests
-		beautifulsoup4
-		selenium
+	buildInputs = with pkgs; [
+		python3Packages.black
+		python3Packages.flask
+		python3Packages.flask_sqlalchemy
+		python3Packages.requests
+		python3Packages.beautifulsoup4
+		python3Packages.selenium
+		python3Packages.psycopg2
+		postgresql
 	];
 }
